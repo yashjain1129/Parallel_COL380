bcf5a152f27608acb677c603c657e3ca6a4990f7
yashjain1129 <39984996+yashjain1129@users.noreply.github.com>
2020-04-11 10:39:58 +0530

    io format corrected

--- pgrank.cpp
+++ pgrank.cpp
@@ -4,36 +4,48 @@ using namespace std;
 
 struct webpage{
 	int num_outgoing =0;
-	float pagerank = 1;
+	double pagerank = 1;
 	vector<int> outlist;
 };
 
-int num_edges=1;
-int num_webpages=1;
+int num_edges=0;
+int num_webpages=20000;
 
-vector<vector<float>> intermediate_pg(100000);
+vector<vector<double>> intermediate_pg(100000);
 vector<vector<int>> outgoing_links(100000);
-vector<float> pageranks(100000, 0.0f);
-vector<float> pageranks_up(100000, 0.0f);
+vector<double> pageranks(100000, 0.0f);
+vector<double> pageranks_up(100000, 0.0f);
+vector<double> sumlist;
 
-void emit(int key, float value){
+void emit1(int key, double value){
+	sumlist.push_back(value);
+}
+
+void mapper1(int key, double pg){
+	int sizes = outgoing_links[key].size();
+	if(sizes==0){
+		emit1(1,pg);
+	}
+}
+
+void emit(int key, double value){
 	intermediate_pg[key].push_back(value);
 }
 
-void mapper(int key, float pg){
+void mapper(int key, double pg){
 	emit(key, 0.0f);
 	int sizes = outgoing_links[key].size();
 	if(sizes!=0){
 		for(int i =0; i<sizes; i++){
-			emit(outgoing_links[key][i],(float)(pg/sizes));
+			emit(outgoing_links[key][i],(double)(pg/sizes));
 		}
 	}
 }
 
-void reducer(int key, vector<float> pg){
-	float up_pg = 0.0f;
+void reducer(int key, vector<double> pg){
+	double up_pg = 0.0f;
 	for(int i =0; i<pg.size(); i++){
-		up_pg += (float)pg[i];
+		up_pg += (double)pg[i];
 	}
 	pageranks_up[key] = up_pg;
 }
@@ -41,56 +53,67 @@ void reducer(int key, vector<float> pg){
 int main(int argc, char const *argv[])
 {
 
-	float alpha = 0.85;
-	float conv = 0.0001;
+	double alpha = 0.85;
+	double conv = 0.00001;
+	int a,b,i;
 
-	cin >> num_webpages;
-	cin >> num_edges;
+	ifstream fopen;
+	fopen.close();
+	fopen.open("barabasi-20000.txt");
+	while(!fopen.eof()){
+		fopen>>a>>b;
+		outgoing_links[a].push_back(b);
+	}
+	fopen.close();
 
 	// struct webpage w1;
-	float pgr = float(1.0f/num_webpages);
+	double pgr = double(1.0f/num_webpages);
 	for(int i=0; i<num_webpages; i++){
 		pageranks[i] = pgr;
 	}
-	// vector<webpage> graph_webpage(num_webpages, w1);
 
-	int a,b,i;
-
-	for(i=0; i<num_edges; i++){
-		cin>>a>>b;
-		outgoing_links[a].push_back(b);
-	}
-
+	int count = 0;
 	while(true){
+		for(i=0; i<num_webpages; i++){
+			mapper1(i,pageranks[i]);
+		}
+		double dp =0.0f;
+		double ap =0.0f;
+		for(i=0; i<sumlist.size(); i++){
+			dp += sumlist[i];
+		}
+		dp = double(dp/num_webpages);
+		sumlist.clear();
+		for(i=0; i<num_webpages; i++){
+			ap += pageranks[i];
+		}						
+		ap = double(ap/num_webpages);
 		for(i=0; i<num_webpages; i++){
 			mapper(i,pageranks[i]);
 		}
 		for(i=0; i<num_webpages; i++){
 			reducer(i,intermediate_pg[i]);
-			for(int j=0; j<intermediate_pg[i].size(); j++){
-				cout<<intermediate_pg[i][j]<<" ";
-			}
 			intermediate_pg[i].clear();
 		}	
+		for(i=0; i<num_webpages; i++){
+			pageranks_up[i] = alpha*pageranks_up[i] + alpha*dp + (1-alpha)*ap;
+		}
 		bool converging = true;
 		for(i=0; i<num_webpages; i++){
 			if(pageranks[i]-pageranks_up[i]>conv)
 				converging=false;
 			pageranks[i] = pageranks_up[i];
 		}
-		for(int i=0; i<num_webpages; i++){
-			cout<<pageranks[i]<<" ";
-		}
-
 		if(converging)
 			break;
 	}
 
-	cout<<"--------here is the final output------------"<<endl;
-
+	double sumer = 0.0;
 	for(int i=0; i<num_webpages; i++){
-		cout<<pageranks[i]<<" ";
+		cout<<i<<" = "<<pageranks[i]<<endl;
+		sumer += pageranks[i];
 	}
+	cout<<"sum "<<sumer;
 
 	/* code */
 	return 0;
