d9cf33ff49b7910cbc31c8731aff28e9e73b0054
TANMAY912 <tanudim2012@gmail.com>
2020-05-04 19:42:10 +0530

    Update test.cpp

--- final/part 2/test.cpp
+++ final/part 2/test.cpp
@@ -16,24 +16,24 @@ int main(int argc,  char **argv) {
   MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
 
   double alpha = 0.85;
-	double conv = 0.00001;
-	int a,b,i;
-	ifstream fopen;
-	fopen.close();
-	fopen.open(argv[1]);
-	while(!fopen.eof()){
-		fopen>>a>>b;
-		num_webpages = max(num_webpages,max(a,b));
+  double conv = 0.00001;
+  int a,b,i;
+  ifstream fopen;
+  fopen.close();
+  fopen.open(argv[1]);
+  while(!fopen.eof()){
+    fopen>>a>>b;
+    num_webpages = max(num_webpages,max(a,b));
     outgoing_links[a].push_back(b);
-	}
-	fopen.close();
-	num_webpages++;
+  }
+  fopen.close();
+  num_webpages++;
   // num_webpages = 20000;
 
   double pgr = double(1.0f/num_webpages);
-	for(int i=0; i<num_webpages; i++){
-		pageranks[i] = pgr;
-	}
+  for(int i=0; i<num_webpages; i++){
+    pageranks[i] = pgr;
+  }
   int tt = 0;
 
   mprdc mr;
@@ -42,7 +42,7 @@ int main(int argc,  char **argv) {
     double dp =0.0f;
     for(i=0; i<num_webpages; i++){
       if(outgoing_links[i].size()==0){
-        dp += pageranks[i];	// sumlist.push_back(value);
+        dp += pageranks[i];  // sumlist.push_back(value);
       }
     }
     dp = double(dp/num_webpages);
@@ -63,31 +63,31 @@ int main(int argc,  char **argv) {
       ttt++;
     }
     for(i=0; i<num_webpages; i++){
-			pageranks_up[i] = (double)alpha*pageranks_up[i] + alpha*dp + (double)(1-alpha)/num_webpages;
-		}
+      pageranks_up[i] = (double)alpha*pageranks_up[i] + alpha*dp + (double)(1-alpha)/num_webpages;
+    }
     bool converging = true;
-		for(i=0; i<num_webpages; i++){
-			if(pageranks[i]-pageranks_up[i]>conv)
-				converging=false;
+    for(i=0; i<num_webpages; i++){
+      if(pageranks[i]-pageranks_up[i]>conv){
+        converging=false;
         pageranks[i] = pageranks_up[i];
-  			// pageranks_up[i] = 0;
-		}
+      }
+    }
 
     /*
     double sumer = 0.0;
-  	for(int i=0; i<num_webpages; i++){
-  		// cout<<i<<" = "<<pageranks[i]<<endl;
-  		sumer += pageranks[i];
-  	}
-  	cout<<"sum "<<sumer << endl;
+    for(int i=0; i<num_webpages; i++){
+      // cout<<i<<" = "<<pageranks[i]<<endl;
+      sumer += pageranks[i];
+    }
+    cout<<"sum "<<sumer << endl;
     */
 
     // if(me == 0){
     //   std::cout << "pageranks[0] = " << dp << '\n';
     // }
     // cout << "Loop -> " << tt << " is " << converging << endl;
-		if(converging)
-			break;
+    if(converging)
+      break;
 
     tt++;
 
@@ -100,19 +100,25 @@ int main(int argc,  char **argv) {
   }
 
   double sumer = 0.0;
-	for(int i=0; i<num_webpages; i++){
+  for(int i=0; i<num_webpages; i++){
     if(me == 0){
       // cout << i << " = " << pageranks[i] << endl;
     }
-		sumer += pageranks[i];
-	}
+    sumer += pageranks[i];
+  }
   */
 
   if(me == 0){
-    pageranks.resize(num_webpages);
-    std::ofstream output_file(argv[3]);
-    std::ostream_iterator<double> output_iterator(output_file, "\n");
-    std::copy(pageranks.begin(), pageranks.end(), output_iterator);
+    ofstream fopen1;
+    fopen1.open(argv[3]);
+    double sumer = 0.0;
+    for(int i=0; i<num_webpages; i++){
+        fopen1<<i<<" = "<<pageranks[i]<<endl;
+        sumer += pageranks[i];
+    }
+    // cout<<"number of iterations are "<<count<<endl;
+    fopen1<<"sum "<<sumer;
+    fopen1.close();
   }
 
   MPI_Finalize();
@@ -126,6 +132,7 @@ for windows subsystem warning:
 echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
 
 compile:
+make -f Makefile.part2
 mpic++ -g -Wall -o mr-pr-mpi.o test.cpp mylib.cpp mylib.h
 
 run:
